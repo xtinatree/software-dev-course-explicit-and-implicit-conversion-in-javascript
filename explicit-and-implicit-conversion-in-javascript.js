406bc267dbd7954c5aa39600539299703960e4dc
Cristina <cristinahernandez510@gmail.com>
2025-07-10 18:46:05 -0500

    Committed my fixes and examples

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -20,13 +20,43 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 
 let result = "5" - 2;
-console.log("The result is: " + result);
+console.log("The result is: " + result); //this is an example of implicit conversion
 
-let isValid = Boolean("false");
+//the result is:3
+
+
+let isValid = Boolean(false); //I deleted the quotation marks, did not use explicit conversion methods to return the 
+
+    console.log(Boolean(isValid)); //I used this to check if the boolean value was falsy or truthy, it returned true orginally
+
+
 if (isValid) {
     console.log("This is valid!");
 }
 
+//false
+
 let age = "25";
-let totalAge = age + 5;
+let ageNumber = Number(age); //This converts the string age into a number
+let totalAge = ageNumber + 5; //Now instead of concatenating, it adds together the sum to produce 30 instead of 255
 console.log("Total Age: " + totalAge);
+//Total Age: 30
+
+
+//My examples
+
+//Implicit conversion example
+let numberOfApples = 5
+let totalOfApples = numberOfApples - 2;
+console.log("Apples Eaten: " + totalOfApples);
+
+//Explicit conversion example
+let numberOfBananas = "5"
+let totalOfBananas = Number(numberOfBananas) + 2;
+
+console.log("Bananas Eaten: " + totalOfBananas); //this returns 52, before using parseInt, after it returns 7
+
+//edge case example
+
+let frogs = 0;
+console.log(Boolean(frogs));
